d0b3000ea651707714c11061f7ccdf8195aea27e
yo16 <xxyo1xx@gmail.com>
2024-04-23 14:08:04 +0900

    色とかの微調整

--- src/main3.py
+++ src/main3.py
@@ -10,9 +10,11 @@ def get_corner_points(cv_image_original):
 
     # 輪郭を検出するための準備：前処理としてマスク画像を取得する
     hsv = cv2.cvtColor(cv_image, cv2.COLOR_BGR2HSV)
-    lower_blue = np.array([100, 150, 50])  # 青色のHSV範囲の下限
-    upper_blue = np.array([140, 255, 255])  # 青色のHSV範囲の上限
+    lower_blue = np.array([90, 50, 50])  # 青色のHSV範囲の下限
+    upper_blue = np.array([140, 255, 255])  # 青色のHSV範囲の上限191
     mask = cv2.inRange(hsv, lower_blue, upper_blue)
+    output_path = './tmp/corner_mask.jpg'
+    cv2.imwrite(output_path, mask)
     contours, _ = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     # 点を左上、右上、右下、左下の順に並べ替える関数
@@ -41,6 +43,7 @@ def get_corner_points(cv_image_original):
     epsilon = 0.02 * perimeter
     approx = cv2.approxPolyDP(largest_contour, epsilon, True)
 
+    assert len(approx) == 4, '角が４点見つかっていません！(' + str(len(approx)) + ')'
     # 近似した頂点が4つであれば、それらは四角形の角
     if len(approx) == 4:
         cv2.drawContours(cv_image, [approx], -1, (0, 255, 0), 5)  # 輪郭を緑色で描画
@@ -63,12 +66,15 @@ class Color(Enum):
 def get_ball_positions(cv_image_original, vertices):
 
     def get_ball_position_by_color(cv_image, vertices, color):
+        color_str = 'red' if (color==Color.RED) else 'yellow' if (color==Color.YELLOW) else 'white'
+
         hsv = cv2.cvtColor(cv_image, cv2.COLOR_BGR2HSV)
+        mask = None
         if (color == Color.RED):
             # 赤色のHSV範囲
             lower_red1 = np.array([0, 100, 50])
             upper_red1 = np.array([10, 255, 255])
-            lower_red2 = np.array([165, 100, 50])
+            lower_red2 = np.array([165, 50, 50])
             upper_red2 = np.array([180, 255, 255])
 
             # 赤色は、HSVで２か所で出るため、ORで結合する
@@ -77,6 +83,7 @@ def get_ball_positions(cv_image_original, vertices):
             mask = cv2.bitwise_or(mask1, mask2)
             cv2.imwrite('./tmp/detect_ball_red_mask1.jpg', mask1)
             cv2.imwrite('./tmp/detect_ball_red_mask2.jpg', mask2)
+            cv2.imwrite('./tmp/detect_ball_red_mask3.jpg', mask)
 
         elif (color == Color.YELLOW):
             # 黄色のHSV範囲
@@ -85,12 +92,13 @@ def get_ball_positions(cv_image_original, vertices):
             lower_yellow = np.array([15, 0, 0])
             upper_yellow = np.array([30, 255, 255])
             mask = cv2.inRange(hsv, lower_yellow, upper_yellow)
+            cv2.imwrite('./tmp/detect_ball_yellow_mask.jpg', mask)
         else:
             # 白色のHSV範囲
-            lower_white = np.array([0, 0, 200])
-            upper_white = np.array([180, 70, 255])
+            lower_white = np.array([0, 0, 50])
+            upper_white = np.array([180, 150, 255])
             mask = cv2.inRange(hsv, lower_white, upper_white)
-        cv2.imwrite('./tmp/detect_ball_mask.jpg', mask)
+            cv2.imwrite('./tmp/detect_ball_white_mask.jpg', mask)
 
         # 指定された四角形領域でマスクを適用
         #cv2.fillPoly(mask, [vertices], 0)
@@ -100,24 +108,25 @@ def get_ball_positions(cv_image_original, vertices):
         cv2.fillPoly(rect_mask, [vertices], 255)  # 矩形内部を255で塗りつぶす
         masked_image = cv2.bitwise_and(mask, rect_mask)  # 元のマスクと矩形マスクのANDを取る
 
-        cv2.imwrite('./tmp/detect_ball_rect.jpg', masked_image)
+        cv2.imwrite('./tmp/detect_ball_rect_'+color_str+'.jpg', masked_image)
 
         # オープニング（縮小して拡大、ノイズ除去）
         kernel1 = np.ones((5,5), np.uint8)
         opening_image = cv2.morphologyEx(masked_image, cv2.MORPH_OPEN, kernel1)
-        cv2.imwrite('./tmp/detect_ball_open.jpg', opening_image)
+        cv2.imwrite('./tmp/detect_ball_open_'+color_str+'.jpg', opening_image)
 
         # クロージング（拡大して縮小、穴埋め）
         kernel2 = np.ones((5,5), np.uint8)
         closing_image = cv2.morphologyEx(opening_image, cv2.MORPH_CLOSE, kernel2)
-        cv2.imwrite('./tmp/detect_ball_close.jpg', closing_image)
+        cv2.imwrite('./tmp/detect_ball_close_'+color_str+'.jpg', closing_image)
 
         # 球の検出
         balls = []
         contours, _ = cv2.findContours(closing_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         for contour in contours:
             ((x, y), radius) = cv2.minEnclosingCircle(contour)
-            if radius > 10:  # 最小サイズのしきい値
+            print(radius)
+            if radius > 2:  # 最小サイズのしきい値
                 balls.append((int(x), int(y), int(radius)))
         
         return np.array(balls)
@@ -127,8 +136,11 @@ def get_ball_positions(cv_image_original, vertices):
 
     # 赤、白、黄色のボールの位置を取得
     red_balls = get_ball_position_by_color(cv_image, vertices, Color.RED)
+    assert len(red_balls)>0, '赤玉が見つかりません'
     white_balls = get_ball_position_by_color(cv_image, vertices, Color.WHITE)
+    assert len(white_balls)>0, '白玉が見つかりません'
     yellow_balls = get_ball_position_by_color(cv_image, vertices, Color.YELLOW)
+    assert len(yellow_balls)>0, '黄玉が見つかりません'
 
     # 位置を描画
     for ball in red_balls:
@@ -203,18 +215,19 @@ def cut_and_transform(original_image, points, width, height, balls, ball_size):
 
 
 
-def main():
+def main(image_path):
     # 画像を読み込む
-    image_path = './images/sample1.jpg'
     image_scale = 2.0
-    width = int(160 * image_scale)
-    height = int(290 * image_scale)
+    width = int(142 * image_scale)
+    height = int(284 * image_scale)
     ball_size = 6.15 * image_scale
 
     original_image = cv2.imread(image_path)
 
     # ビリヤード台の角を取得
     corners = get_corner_points(original_image)
+    print("corners")
+    print(corners)
 
     # ボールの位置を取得
     rwy_balls = get_ball_positions(original_image, corners)
@@ -227,4 +240,6 @@ def main():
 
 
 if __name__=="__main__":
-    main()
+    image_path = './images/sample1.jpg'
+    main(image_path)
+
