1ff97381e6aa66cdf557633ea62975fd75e99c8d
yo16 <xxyo1xx@gmail.com>
2024-05-05 14:40:13 +0900

    台の青の調整

--- src/main3.py
+++ src/main3.py
@@ -12,12 +12,20 @@ def get_corner_points(cv_image_original):
 
     # 輪郭を検出するための準備：前処理としてマスク画像を取得する
     hsv = cv2.cvtColor(cv_image, cv2.COLOR_BGR2HSV)
-    lower_blue = np.array([90, 50, 50])  # 青色のHSV範囲の下限
-    upper_blue = np.array([140, 255, 255])  # 青色のHSV範囲の上限191
+    lower_blue = np.array([80, 10, 50])  # 青色のHSV範囲の下限
+    upper_blue = np.array([165, 255, 255])  # 青色のHSV範囲の上限191
     mask = cv2.inRange(hsv, lower_blue, upper_blue)
     output_path = './tmp/corner_mask.jpg'
     cv2.imwrite(output_path, mask)
     contours, _ = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cv_image_contours = cv_image_original.copy()
+    #for cont in contours:
+    #    print(cont)
+    #    cv2.circle(cv_image_contours, (int(cont[0]), int(cont[1])), 1, (0,255,0), 1)
+    cv2.drawContours(cv_image_contours, contours, -1, (0,0,0), 1)
+    output_path = './tmp/table_contours.jpg'
+    cv2.imwrite(output_path, cv_image_contours)
+    
 
     # 点を左上、右上、右下、左下の順に並べ替える関数
     # 1. Y座標の小さな上位２つが上、大きな上位２つが下
@@ -48,7 +56,7 @@ def get_corner_points(cv_image_original):
     assert len(approx) == 4, '角が４点見つかっていません！(' + str(len(approx)) + ')'
     # 近似した頂点が4つであれば、それらは四角形の角
     if len(approx) == 4:
-        cv2.drawContours(cv_image, [approx], -1, (0, 255, 0), 5)  # 輪郭を緑色で描画
+        cv2.drawContours(cv_image, [approx], -1, (0, 255, 0), 1)  # 輪郭を緑色で描画
         approx = approx.reshape(4, 2)
         approx = order_points(approx)  # 頂点を並べ替える
 
@@ -174,7 +182,14 @@ def get_ball_positions(cv_image_original, vertices):
 
 
 # perspective transformationで長方形へ変換
-def cut_and_transform(original_image, points, short_hand_width, long_hand_width, balls, ball_r, out_image_path):
+def cut_and_transform(
+    original_image,
+    points,
+    short_hand_width,
+    long_hand_width,
+    balls, ball_r,
+    out_image_path
+):
     src_points = np.float32(points)
     print(src_points)
 
@@ -256,7 +271,11 @@ def main(image_path, out_image_path):
     print(rwy_balls)
 
     # perspective transformationで長方形へ変換
-    cut_and_transform(original_image, corners, short_hand_width, long_hand_width, rwy_balls, ball_r, out_image_path)
+    cut_and_transform(
+        original_image, corners,
+        short_hand_width, long_hand_width,
+        rwy_balls, ball_r, out_image_path
+    )
 
 
 
